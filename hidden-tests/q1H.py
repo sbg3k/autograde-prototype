2e43cd0400f7f6b59c76c866932c15b7bf2779c3
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-01 22:41:55 +0100

    Add files via upload

---
+++ hidden-tests/q1H.py
@@ -0,0 +1,23 @@
+test = {
+	"name": "q1H",
+	"points": 4,
+	"hidden": True,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> main(5, 44) == 9
+					False
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}
+	]
+}
