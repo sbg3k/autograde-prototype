e69fa7cac68701027e52b823f5c05133dba9eb4b
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-02 23:11:22 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = ''
-    UPLOAD_FOLDER = "./hidden-tests"
-    PASS='lcv:>?;W5QD<,js8oKyE4q`nN"z$M.?ts4#zd3o^n+9bk%6q$1@1P+P6uD^fY0z.>'
-    ALLOWED_EXTENSION = "py"
 
 
 class ProductionConfig(Config):
