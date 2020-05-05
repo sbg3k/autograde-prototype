df28e486eb3b6bfa8930edd17d15851a1fa98026
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-05 11:48:31 +0100

    Add files via upload

---
+++ hidden-tests/test4.py
@@ -0,0 +1,24 @@
+test = {
+	"name": "test4",
+	"points": 3,
+	"hidden": True,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> wedding_chow('RSMFD'))
+					(0, '')
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}
+	]
+}
+
