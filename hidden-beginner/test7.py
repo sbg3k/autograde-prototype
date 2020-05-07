048ff1217eda3798d973121909cf69340430d19b
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-07 09:24:52 +0100

    Update test7.py

--- hidden-beginner/test7.py
+++ hidden-beginner/test7.py
@@ -1,1 +1,23 @@
-A
+test = {
+	"name": "test7",
+	"points": 2,
+	"hidden": True,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> averageMultiple.__doc__ != None
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}
+	]
+}
