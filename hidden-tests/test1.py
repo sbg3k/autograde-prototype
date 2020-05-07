d2617efbe5654cce0e5ded4fda9558841909e375
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-07 14:41:24 +0100

    Add files via upload

--- hidden-tests/test1.py
+++ hidden-tests/test1.py
@@ -1,14 +1,28 @@
 test = {
 	"name": "test1",
-	"points":1,
+	"points": 1,
 	"hidden": True,
 	"suites": [ 
 		{
 			"cases": [ 
 				{
 					"code": r"""
-					>>> wedding_chow('rsmdmdsrffd')
-					(2, 'd')
+					>>> "order" in dir()
+					True
+					>>> order([1, 4, 2]) == (2, [4])
+					True
+					>>> def errorr(s):
+					...	 try:
+					...		 x=order(s)
+					...	 except AssertionError:
+					...		 return True
+					...	 except:
+					...		 return False
+					...	 else:
+					...		 return x
+					...
+					>>> errorr((100,1,2,3,21,22,23,200,31,32,33,4))
+					(4, [21, 22, 23, 31, 32, 33, 100, 200])
 					""",
 					"hidden": False,
 					"locked": False,
@@ -21,4 +35,3 @@ test = {
 		}
 	]
 }
-
