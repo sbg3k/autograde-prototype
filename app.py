7c1eb5801846037ac97c3cce91ca7163fa51ccda
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-07 02:54:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,10 +46,8 @@ def upload_file():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(UPLOAD_FOLDER[request.form['level']][:2], filename))
 			if True:
-				loc = {}
-				exec('from ' + filename[:-3] + ' import *', globals(), loc)
+				exec('from ' + filename[:-3] + ' import *', globals())
 				print(filename)
-				print(loc['order']([1, 1]))
 				print("executing...", "[", request.form['name'], "]")
 				with open(os.path.join(UPLOAD_FOLDER[request.form['level']][:2], filename)) as f:
 					data = f.read().replace('\n', '')
