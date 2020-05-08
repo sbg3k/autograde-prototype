58fc8c495e672e0ad912e045c610e73ddfb6eabb
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-08 23:23:38 +0100

    Update test5.py

--- hidden-beginner/test5.py
+++ hidden-beginner/test5.py
@@ -9,7 +9,17 @@ test = {
 					"code": r"""
 					>>> "fastSum" in dir()
 					True
-					>>> fastSum(50505050) == 1275380063003775
+					>>> from multiprocessing import Process
+					>>> def run_with_limited_time(func, args, kwargs, time):
+					...     p = Process(target=func, args=args, kwargs=kwargs)
+					...     p.start()
+					...     p.join(time)
+					...     if p.is_alive():
+					...         p.terminate()
+					...         return False
+					...     return fastSum(50505050)
+					... 
+					>>> run_with_limited_time(fastSum, (50505050,), {}, 5) == 1275380063003775
 					True
 					""",
 					"hidden": False,
