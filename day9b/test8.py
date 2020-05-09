9d1c3a7f7d3da6f97f6c44e6da634d4eaa20495c
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-09 15:05:19 +0100

    Add files via upload

---
+++ day9b/test8.py
@@ -0,0 +1,23 @@
+test = {
+	"name": "test8",
+	"points": 1,
+	"hidden": True,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> 1 == 1
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}
+	]
+}
