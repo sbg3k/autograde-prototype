544437c9192393ff22bb0daa146f49d5f0d91da1
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-11 19:18:23 +0100

    Update test8.py

--- hidden-tests/test8.py
+++ hidden-tests/test8.py
@@ -30,24 +30,6 @@ test = {
                     "hidden": False,
                     "locked": False,
                 },
-                {
-                    "code": r"""
-                    >>> def errorr(s,b):
-                    ...     try:
-                    ...         x=bin(s,b)
-                    ...     except AssertionError:
-                    ...         return True
-                    ...     except:
-                    ...         return False
-                    ...     else:
-                    ...         return x
-                    ...
-                    >>> errorr(range(128), ['1', '2', '4', '8', '16', '32', '64']) and errorr([],[])[0]==0
-                    True
-                    """,
-                    "hidden": False,
-                    "locked": False,
-                },
             ],
             "scored": False,
             "setup": "",
