19af4ae0788aee3f4de161398311279a8b4b370a
Aaron <55332143+nerdyphil@users.noreply.github.com>
2020-05-11 16:16:39 +0100

    Add files via upload

--- hidden-beginner/test1.py
+++ hidden-beginner/test1.py
@@ -1,24 +1,31 @@
 test = {
-	"name": "test1",
-	"points": 1,
-	"hidden": True,
-	"suites": [
-		{
-			"cases": [
-				{
-					"code": r"""
-					>>> assert "fibonacci" in dir()
-					>>> fibonacci(10946)
-					True
-					""",
-					"hidden": False,
-					"locked": False,
-				},
-			],
-			"scored": False,
-			"setup": "",
-			"teardown": "",
-			"type": "doctest"
-		}
-	]
+    "name": "test4",
+    "points": 1,
+    "hidden": True,
+    "suites": [
+        {
+            "cases": [
+                {
+                    "code": r"""
+                    >>> 'squareSum' in dir()
+                    True
+                    """,
+                    "hidden": False,
+                    "locked": False,
+                },
+                {
+                    "code": r"""
+                    >>> squareSum(10)==2640
+                    True
+                    """,
+                    "hidden": False,
+                    "locked": False,
+                },
+            ],
+            "scored": False,
+            "setup": "",
+            "teardown": "",
+            "type": "doctest"
+        }
+    ]
 }
