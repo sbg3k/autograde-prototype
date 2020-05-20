7162c5df36f9399c4ec3a3c99b0d78948b0872da
nerdyphil <owoeyepo@gmail.com>
2020-05-20 16:49:13 +0100

    modified:   hidden-beginner/test8.py

--- hidden-beginner/test8.py
+++ hidden-beginner/test8.py
@@ -21,16 +21,3 @@ test = {
 		}
 	]
 }
-DAY 20 TEST CASE
-1 point for submission
-create_arr(3, 5)==[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]] # 2 points
-create_arr(1, 3)==[[0, 0, 0]] # 2 points
-create_arr(4, 13)==[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24], [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]] # 3 points
-create_arr.__doc__ != # 1 point
-def error(func, num, n):
-   try:
-       return func(num, n)
-   except AssertionError:
-       return 1
-error(create_arr, 'a', -1)==1 # 3 points
-error(create_arr, -1, -2) ==1 #3points
